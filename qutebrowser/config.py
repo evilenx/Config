4aace40d16b359effdea577d217c68dd66a2d81c
evilenx <evilenx@proton.me>
2025-11-29 02:28:18 -0500

    latest update youtube.ads and configurations

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,12 +5,24 @@ config.load_autoconfig()
 c.url.default_page = 'https://www.google.com' # new tab 
 c.url.start_pages = 'https://www.google.com'
 
+#c.url.searchengines = {
+#    'DEFAULT': 'https://www.google.com/search?q={}',
+#    'g': 'https://www.google.com/search?q={}',
+#    'ddg': 'https://wwww.google.com/?q={}',
+#    # Add more search engines here
+#}
+
 c.url.searchengines = {
-    'DEFAULT': 'https://www.google.com/search?q={}',
-    'g': 'https://www.google.com/search?q={}',
-    'ddg': 'https://wwww.google.com/?q={}',
-    # Add more search engines here
+    # DEFAULT y g ahora fuerzan la búsqueda SOLAMENTE en la Web
+    'DEFAULT': 'https://www.google.com/search?udm=14&q={}&hl=en&gl=US',
+    'g': 'https://www.google.com/search?udm=14&q={}&hl=en&gl=US',
+    
+    'ddg': 'https://duckduckgo.com/?q={}',
 }
+
+
+
+
 
 #config.bind('e', 'edit-url ;; open')
 
@@ -29,32 +41,36 @@ c.content.blocking.enabled = True
 
 c.content.blocking.method = 'adblock'
 
+c.content.blocking.enabled = True
+# c.content.blocking.method = 'adblock' # uncomment this if you install python-adblock
 c.content.blocking.adblock.lists = [
-     "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
-     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
-
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
 
 # zoom
 c.zoom.default = '75%'
 
 
+
 # Bind a key to translate the page (useful for Google Translate)
-# Bind a key to translate the page using DeepL
-config.bind('T', 'spawn --detach xdg-open "https://www.deepl.com/translator#en/es/{url}"')
+config.bind('T', 'open https://translate.google.com/translate?sl=auto&tl=es&u={url}')
+
+
+
+
 
